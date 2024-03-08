13d238ccc273f9e938175ada2c9aa66bad8719b2
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-03-08 14:25:48 +0800

    Added phone number

--- components/connectionModal.jsx
+++ components/connectionModal.jsx
@@ -32,6 +32,8 @@ const ConnectionModal = ({
   const [slaveName, setSlaveName] = useState("");
   const [newMasterName, setNewMasterName] = useState("");
   const [newSlaveName, setNewSlaveName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [newPhone, setNewPhone] = useState("");
   const [newUid, setNewUid] = useState();
   const [radius, setRadius] = useState("");
   const [newRadius, setNewRadius] = useState("");
@@ -43,11 +45,13 @@ const ConnectionModal = ({
   const masterRef = ref(database, `uids/${uid}/masterName`);
   const slaveRef = ref(database, `uids/${uid}/slaveName`);
   const radiusRef = ref(database, `uids/${uid}/radius`);
+  const phoneRef = ref(database, `uids/${uid}/phone`);
 
   const masterInputRef = useRef();
   const slaveInputRef = useRef();
   const deviceRef = useRef();
   const radiusInputRef = useRef();
+  const phoneInputRef = useRef();
 
   useEffect(() => {
     onValue(masterRef, (snapshot) => {
@@ -65,12 +69,18 @@ const ConnectionModal = ({
       setRadius(data);
       updateRadius(data);
     });
+    onValue(phoneRef, (snapshot) => {
+      const data = snapshot.val();
+      setPhone(data);
+    });
   }, [uid]);
 
   const blur = () => {
     masterInputRef.current.blur();
     slaveInputRef.current.blur();
     radiusInputRef.current.blur();
+    phoneInputRef.current.blur();
+    closeModal();
   };
 
   const handleUpdateInfo = () => {
@@ -87,6 +97,10 @@ const ConnectionModal = ({
       slaveName: newSlaveName.length <= 0 ? slaveName : newSlaveName.toString(),
     });
 
+    update(ref(database, `/uids/${uid}`), {
+      phone: newPhone.length <= 0 ? phone : newPhone.toString(),
+    });
+
     showToast("success", "Updated Successfully.");
     blur();
   };
@@ -97,7 +111,8 @@ const ConnectionModal = ({
     } else {
       deviceRef.current.blur();
       setShowScanner(false);
-      showToast("error", "Device uid is not a number.");
+      closeModal();
+      showToast("error", `Device UID: ${uid} is not a number.`);
     }
   };
 
@@ -119,9 +134,10 @@ const ConnectionModal = ({
           found = true;
         } else {
           if (!found) {
-            showToast("error", "Device UID is not exist.");
+            showToast("error", `Device UID: ${uid} is not exist.`);
             deviceRef.current.blur();
             setShowScanner(false);
+            closeModal();
           }
         }
       });
@@ -240,6 +256,45 @@ const ConnectionModal = ({
                         bgColor={"#232D3F"}
                       />
                     </View>
+                  </View>
+                </View>
+                <View style={{ paddingHorizontal: 10, marginTop: 15 }}>
+                  <Text style={{ color: "gray", marginBottom: 3 }}>
+                    Contact Number
+                  </Text>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      shadowColor: "#000",
+                      shadowOffset: { width: 0, height: 1 },
+                      shadowOpacity: 0.8,
+                      shadowRadius: 2,
+                      elevation: 3,
+                      borderRadius: 3,
+                      paddingHorizontal: 10,
+                      backgroundColor: "white",
+                      borderRadius: 10,
+                    }}
+                  >
+                    <MaterialCommunityIcons
+                      name="phone"
+                      size={24}
+                      color="gray"
+                    />
+                    <TextInput
+                      inputMode="numeric"
+                      ref={phoneInputRef}
+                      onChangeText={(text) => setNewPhone(text)}
+                      placeholder={
+                        phone == undefined ? "Unknown" : phone.toString()
+                      }
+                      style={{
+                        flex: 1,
+                        paddingVertical: 9,
+                        paddingHorizontal: 10,
+                      }}
+                    />
                   </View>
                 </View>
                 <View style={{ paddingHorizontal: 10, marginTop: 15 }}>
@@ -399,7 +454,7 @@ const styles = StyleSheet.create({
   modalView: {
     backgroundColor: "#FAF5FC",
     width: "100%",
-    height: 620,
+    height: 700,
     padding: 35,
     alignItems: "center",
     shadowColor: "#000",
